e9b1d40f7034ef2a1e464c9ab13fd6963b3b35ab
Roy Kim <roykim@Roys-MacBook-Pro.local>
2022-10-15 22:44:57 -0400

    Organized data parsed from API in pandas dataframe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 import requests
+import datetime
+import numpy as np
+import pandas as pd
 
 load_dotenv()
 
@@ -23,13 +26,22 @@ response = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth
 TOKEN = response.json()["access_token"]
 headers["Authorization"] = f"bearer {TOKEN}"
 
-response = requests.get("https://oauth.reddit.com/r/mechmarket/new", headers=headers, params={"limit" : "1"})
+response = requests.get("https://oauth.reddit.com/r/mechmarket/new", headers=headers, params={"limit" : "3"})
+posts = []
 
 for post in response.json()["data"]["children"]:
-    print(post["data"]["title"])
-    print(post["data"]["author"])
-    print(post["data"]["created"])
-    print("https://reddit.com" + post["data"]["permalink"])
+    posts.append([
+        post["data"]["name"], 
+        post["data"]["title"], 
+        post["data"]["author"], 
+        datetime.date.fromtimestamp(post["data"]["created"]), 
+        ("https://reddit.com/r/mechmarket/comments/" + post["data"]["permalink"]), 
+        post["data"]["selftext"]
+    ])
 
-# print(response.json())
+df = pd.DataFrame(posts, columns=["post_id", "title", "author", "date", "link", "text"])
+
+# to:do
 # Frequnecy of sales/listings of a certain product using panda
+# Valid Locations
+#   [United States]: US [Canada]: CA [Australia]: AU [European Union]: EU [United Kingdom]: UK
